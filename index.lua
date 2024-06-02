fbe89a854bfd9466219f04bdf93d7836c11ea3c1
Incognito Bookmarklet Browser <115047712+craft-o-s-project@users.noreply.github.com>
2024-06-01 20:36:02 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,6 +1,6 @@
 print("----------------------------------------------------------------------------")
 print("cool message")
-print("yes i did add some lua to print these messages")
+print("yes i did add some cool lua scripting to print these messages")
 print("this is so cool")
 print("should i add a button that prints to the console of napture?")
 print("i might make it so you can print what you typed in napture logs")
