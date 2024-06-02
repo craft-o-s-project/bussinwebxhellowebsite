d631aaa77342bb8e00591eaaa537a85edd768e43
rottenpepsi5 <115047712+craft-o-s-project@users.noreply.github.com>
2024-06-01 23:17:25 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,7 +1,11 @@
-local logButton = get("input")  
-local myInput = get("text")      
+-- index.lua (your existing Lua script)
+-- ...
+
+-- Add an event listener to the button
+local logButton = get("logButton")  -- Use 'get' instead of 'document:getElementById'
+local myInput = get("myInput")      -- Use 'get' instead of 'document:getElementById'
 
 logButton:addEventListener("click", function()
     local inputValue = myInput.value
-    print(myInput)
+    print("Input value: " .. inputValue)
 end)
